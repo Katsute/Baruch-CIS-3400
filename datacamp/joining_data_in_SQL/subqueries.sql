2c3e4e99124da1d7debfafcfb838c54dc0261cf9
Katsute <58778985+Katsute@users.noreply.github.com>
2021-10-23 11:49:03 -0400

    Update subqueries.sql

--- datacamp/joining_data_in_SQL/subqueries.sql
+++ datacamp/joining_data_in_SQL/subqueries.sql
@@ -46,3 +46,93 @@ FROM countries,
   	 GROUP BY code) AS sq
     WHERE countries.code = sq.code
 ORDER BY lang_num DESC;
+
+--
+
+SELECT name, continent, inflation_rate
+FROM countries
+INNER JOIN economies
+    USING(code)
+WHERE year = 2015;
+
+SELECT MAX(inflation_rate) AS max_inf
+FROM (
+    SELECT name, continent, inflation_rate
+    FROM countries
+    INNER JOIN economies
+    USING (code)
+    WHERE year = 2015) AS subquery
+GROUP BY continent;
+
+SELECT name, continent, inflation_rate
+FROM countries
+INNER JOIN economies
+    ON countries.code = economies.code
+    WHERE year = 2015
+        AND inflation_rate IN (
+            SELECT MAX(inflation_rate) AS max_inf
+            FROM (
+                SELECT name, continent, inflation_rate
+                FROM countries
+                INNER JOIN economies
+                ON countries.code = economies.code
+                WHERE year = 2015) AS subquery
+            GROUP BY continent);
+
+--
+
+SELECT code, inflation_rate, unemployment_rate
+FROM economies
+WHERE year = 2015 AND
+    code NOT IN (
+        SELECT code
+        FROM countries
+        WHERE (gov_form = 'Constitutional Monarchy' OR gov_form LIKe '%Republic%')
+    )
+ORDER BY inflation_rate;
+
+--
+
+SELECT DISTINCT name, total_investment, imports
+FROM economies AS e
+    LEFT JOIN countries AS c
+        ON (c.code = e.code
+            AND c.code IN (
+            SELECT code
+            FROM languages AS l
+            WHERE l.official = 'true'
+        ) )
+    WHERE region = 'Central America' AND year = 2015
+ORDER BY name;
+
+--
+
+SELECT region, continent, AVG(p.fertility_rate) AS avg_fert_rate
+FROM populations AS p
+    INNER JOIN countries AS c
+        ON c.code = p.country_code
+        WHERE year = 2015
+GROUP BY region, continent
+ORDER BY avg_fert_rate;
+
+--
+
+SELECT
+    name,
+    country_code,
+    city_proper_pop,
+    metroarea_pop,
+    city_proper_pop / metroarea_pop * 100 AS city_perc
+FROM cities
+WHERE
+    name IN (
+        SELECT capital
+        FROM countries
+        WHERE (
+            continent = 'Europe' OR
+            continent LIKE '%America'
+        )
+    ) AND
+    metroarea_pop IS NOT NULL
+ORDER BY city_perc DESC
+LIMIT 10;
